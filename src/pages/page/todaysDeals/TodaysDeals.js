c1c73bf6279245e9b566c7ea2c188a8a56dcb4a4
GANABA-25 <NathanielOwusu01@gmail.com>
2024-07-17 10:02:36 -0700

    fectching Best Deals Products from dataBase

--- src/pages/page/todaysDeals/TodaysDeals.js
+++ src/pages/page/todaysDeals/TodaysDeals.js
@@ -1,242 +1,46 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
+import Lottie from "lottie-react";
+import axios from "axios";
 
-import TodaysDealsProducts from "./TodaysDealsProducts";
-
+import ProductItem from "../../components/ProductItem";
 import ScrollToTop from "../../components/ScrollToTop";
 import NavigationBar from "../../../components/navBar/Navigation";
 import Header_BrandDeals from "./Header_BrandDeals";
-import axios from "axios";
+import loadingAnimation from "../../../lottie/Animation - loading.json";
+import Pagination from "../../components/Pagination";
 import Footer from "../../components/Footer";
-
-const todaysDealsData = [
-  {
-    id: "p1",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541456/FrambegTech/AUDIO%20AND%20VIDEOS/camera_1_oluvj6.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541455/FrambegTech/AUDIO%20AND%20VIDEOS/camera_2_z33h9j.png",
-    productName: "camera",
-    description:
-      "With 20 pre-set cooking modes, you can prepare fresh food in minutes. Enhance your kitchen MS23F301TAK microwave oven",
-    price: 2400,
-    // discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p2",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541997/FrambegTech/TODAYS%20DEALS/camera_1_nkfyzc.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542000/FrambegTech/TODAYS%20DEALS/camera_2_uai3gk.png",
-    productName: "camera2",
-    description:
-      "HP 15s (15s-Eq2143au) is a Windows 11 laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels.",
-    price: 3540,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p3",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542003/FrambegTech/TODAYS%20DEALS/camera_2_1_bed0sr.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542005/FrambegTech/TODAYS%20DEALS/camera_2_2_pjvsjk.png",
-    productName: "camera3",
-    description:
-      "HP 15s (15s-Eq2143au) is a Windows 11 laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels.",
-    price: 4490,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p4",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541982/FrambegTech/TODAYS%20DEALS/big_speakers_1_l4961h.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541984/FrambegTech/TODAYS%20DEALS/big_speakers_2_c12ntj.png",
-    productName: "bigSpeaker",
-    description:
-      "HP 15s (15s-Eq2143au) is a Windows 11 laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels.",
-    price: 2540,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p5",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541987/FrambegTech/TODAYS%20DEALS/bigspeakers_1_a870ez.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541989/FrambegTech/TODAYS%20DEALS/bigspeakers_2_uphmzc.png",
-    productName: "bigSpeaker",
-    description:
-      "HP 15s (15s-Eq2143au) is a Windows 11 laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels.",
-    price: 3440,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p6",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541993/FrambegTech/TODAYS%20DEALS/blenders_1_md4uxe.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541995/FrambegTech/TODAYS%20DEALS/blenders_2_tny7tj.png",
-    productName: "blinder",
-    description:
-      "Bruhm BWF- 070H 7kg Front Load Washing Machine KShs38,200.00.Automatic 7 Kgs 1000 RPM Silver Color.",
-    price: 4590,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p7",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542010/FrambegTech/TODAYS%20DEALS/desktop_1_ubtywf.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542013/FrambegTech/TODAYS%20DEALS/desktop_2_erm5yh.png",
-    productName: "desktop",
-    description:
-      "374L Bottom Freezer Inverter Linear Compressor GC-B459NLXM ; Dimension (mm, WxHxD): 595 x 1,860 x 682.",
-    price: 1440,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p8",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542016/FrambegTech/TODAYS%20DEALS/dishWasher_1_kpefmm.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542018/FrambegTech/TODAYS%20DEALS/dishWasher_2_sgjfs8.png",
-    productName: "dishwasher",
-    description:
-      "374L Bottom Freezer Inverter Linear Compressor GC-B459NLXM ; Dimension (mm, WxHxD): 595 x 1,860 x 682.",
-    price: 2540,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p9",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542021/FrambegTech/TODAYS%20DEALS/iron_1_z7xlyp.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542024/FrambegTech/TODAYS%20DEALS/iron_2_sfkbba.png",
-    productName: "iron",
-    description:
-      "Designed for photographers and videographers alike the Canon EOS 5D Mark IV features a brand new 30.4.",
-    price: 3440,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p10",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542027/FrambegTech/TODAYS%20DEALS/Laptop_1_rybmwz.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542030/FrambegTech/TODAYS%20DEALS/Laptop_2_vsivik.png",
-    productName: "laptop",
-    description:
-      "Galaxy S22 Ultra smartphone offers a 108-megapixel camera and can shoot video in 8K ultra-high-definition resolution.",
-    price: 2240,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p11",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542032/FrambegTech/TODAYS%20DEALS/Laptop_3_ykyfdl.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542036/FrambegTech/TODAYS%20DEALS/Laptop_4_ryxe5k.png",
-    productName: "laptop2",
-    description:
-      "Galaxy S22 Ultra smartphone offers a 108-megapixel camera and can shoot video in 8K ultra-high-definition resolution.",
-    price: 2340,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p12",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542038/FrambegTech/TODAYS%20DEALS/refrigerator_1_ppc3ge.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542041/FrambegTech/TODAYS%20DEALS/refrigerator_2_zwwn9h.png",
-    productName: "refrigerator",
-    description:
-      "Explore 2020 HD T5300 Smart TV with One Remote Function and HDR. Enjoy Full HD resolution with Smart TV powered by TIZEN.",
-    price: 2480,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p13",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542044/FrambegTech/TODAYS%20DEALS/refrigerator_2_1_yfvrih.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542047/FrambegTech/TODAYS%20DEALS/refrigerator_2_2_g1lctt.png",
-    productName: "refrigerator2",
-    description:
-      "Apple iPhone 6 ; Type, IPS LCD ; Size, 4.7 inches, 60.9 cm2 (~65.8% screen-to-body ratio) ; Resolution, 750 x 1334 pixels, 16:9 ratio (~326 ppi density).",
-    price: 2480,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p13",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542051/FrambegTech/TODAYS%20DEALS/refrigerator_3_1_ugriw6.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542053/FrambegTech/TODAYS%20DEALS/refrigerator_3_2_rgfwln.png",
-    productName: "refrigerator3",
-    description:
-      "Apple iPhone 6 ; Type, IPS LCD ; Size, 4.7 inches, 60.9 cm2 (~65.8% screen-to-body ratio) ; Resolution, 750 x 1334 pixels, 16:9 ratio (~326 ppi density).",
-    price: 2480,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p13",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542056/FrambegTech/TODAYS%20DEALS/refrigerator_4_1_opyosv.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542059/FrambegTech/TODAYS%20DEALS/refrigerator_4_2_le29hm.png",
-    productName: "refrigerator4",
-    description:
-      "Apple iPhone 6 ; Type, IPS LCD ; Size, 4.7 inches, 60.9 cm2 (~65.8% screen-to-body ratio) ; Resolution, 750 x 1334 pixels, 16:9 ratio (~326 ppi density).",
-    price: 2480,
-    discount: 2600,
-    addToCart: "refrigerator5",
-  },
-  {
-    id: "p13",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542063/FrambegTech/TODAYS%20DEALS/refrigerator_5_1_pff3zu.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542064/FrambegTech/TODAYS%20DEALS/refrigerator_5_2_gtaekd.png",
-    productName: "refrigerator6",
-    description:
-      "Apple iPhone 6 ; Type, IPS LCD ; Size, 4.7 inches, 60.9 cm2 (~65.8% screen-to-body ratio) ; Resolution, 750 x 1334 pixels, 16:9 ratio (~326 ppi density).",
-    price: 2480,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "p13",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542068/FrambegTech/TODAYS%20DEALS/refrigerator_6_1_sazpp0.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542047/FrambegTech/TODAYS%20DEALS/refrigerator_2_2_g1lctt.png",
-    productName: "refrigerator7",
-    description:
-      "Apple iPhone 6 ; Type, IPS LCD ; Size, 4.7 inches, 60.9 cm2 (~65.8% screen-to-body ratio) ; Resolution, 750 x 1334 pixels, 16:9 ratio (~326 ppi density).",
-    price: 2480,
-    discount: 2600,
-    addToCart: "Add To Cart",
-  },
-];
 
 const TodaysDeals = () => {
   const [searchedWord, setSearchedWord] = useState("");
   const [checkSearchedWord, setCheckedSearchedWord] = useState(false);
+  const [bestDealsProducts, setBestDealsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const filteredProducts = todaysDealsData.filter((product) =>
+  const fetchBestDealsProducts = async (page) => {
+    const category = "Best-Deals";
+    try {
+      const response = await axios.get(
+        ` http://localhost:8080/products/BestDealsProducts/${category}?page=${page}`
+      );
+
+      const { products, totalPages } = response.data;
+      setBestDealsData(products);
+      setTotalPages(totalPages);
+    } catch (error) {
+      console.log("Error Fetching Products", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBestDealsProducts(currentPage + 1);
+  }, [currentPage]);
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
+
+  const filteredProducts = bestDealsProducts.filter((product) =>
     product.productName.toLowerCase().includes(searchedWord.toLowerCase())
   );
 
@@ -256,7 +60,7 @@ const TodaysDeals = () => {
 
         <div className="md:m-4 lg:w-4/5 lg:m-auto">
           <div className="border-2 py-4 md:p-4 lg:p-4 shadow-md bg-white">
-            {checkSearchedWord && filteredProducts.length === 0 ? (
+            {/* {checkSearchedWord && filteredProducts.length === 0 ? (
               <div className="mx-8">
                 <h1 className="text-xl">No results for {searchedWord}</h1>
                 <p>Try checking your spelling or use more general terms</p>
@@ -267,6 +71,40 @@ const TodaysDeals = () => {
                   checkSearchedWord ? filteredProducts : todaysDealsData
                 }
               />
+            )} */}
+
+            {filteredProducts.length === 0 ? (
+              <div className="flex justify-center items-center w-full">
+                <Lottie
+                  className="w-[6rem]"
+                  animationData={loadingAnimation}
+                  loop={true}
+                />
+              </div>
+            ) : (
+              <>
+                <div className="grid grid-cols-2 mx-4 gap-x-4 gap-y-8 md:grid-cols-3 lg:grid-cols-3 lg:mx-0 lg:ml-16">
+                  {filteredProducts?.map((todaysDeals) => (
+                    <ProductItem
+                      key={todaysDeals._id}
+                      id={todaysDeals._id}
+                      productImage={todaysDeals.productImage}
+                      productImage2={todaysDeals.productImage2}
+                      productName={todaysDeals.productName}
+                      description={todaysDeals.description}
+                      price={todaysDeals.price}
+                      discount={todaysDeals.discount}
+                      addToCart={todaysDeals.addToCart}
+                    />
+                  ))}
+                </div>
+                <div>
+                  <Pagination
+                    totalPages={totalPages}
+                    handlePageClick={handlePageClick}
+                  />
+                </div>
+              </>
             )}
           </div>
 
@@ -292,7 +130,7 @@ const TodaysDeals = () => {
                 className="md:p-2 md:mt-4 lg:p-0 lg:mt-0"
                 src="https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711542073/FrambegTech/TODAYS%20DEALS/samsung_vuvucg.jpg"
                 alt="samsungImage"
-              ></img>
+              />
             </div>
           </div>
         </div>
