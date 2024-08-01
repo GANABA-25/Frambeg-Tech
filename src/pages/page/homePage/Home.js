dd41e24961b38492c8d436b72dfdc9aa2ea41086
GANABA-25 <NathanielOwusu01@gmail.com>
2024-08-01 12:04:29 -0700

    Updated

--- src/pages/page/homePage/Home.js
+++ src/pages/page/homePage/Home.js
@@ -1,4 +1,7 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
+import axios from "axios";
 
 import NavigationBar from "../../../components/navBar/Navigation";
 
@@ -6,104 +9,40 @@ import Carousel from "../../components/Carousel";
 import Offers from "./Offers";
 import BrandDeals from "./BrandDeals";
 import AvailableProducts from "./AvailableProducts";
-import TodaysBestDeals from "./TodaysBestDeals";
 import TopBrands from "./TopBrands";
 import CustomerComments from "./CustomerComments";
+import ProductItem from "../../components/ProductItem";
+import loadingAnimation from "../../../lottie/Animation - loading.json";
+import Pagination from "../../components/Pagination";
 import Footer from "../../components/Footer";
-
-const HomePageProducts = [
-  {
-    id: "b1",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541606/FrambegTech/HOME%20PAGE/trimmer_jdadmp.png",
-    productImage2: "",
-    productName: "samsung tv1",
-    description:
-      "The all-in-1 trimmer for your choice of beard, head, body, face hairstyling and other trimming needs. To deliver maximum power,",
-    price: 2400,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "b2",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541603/FrambegTech/HOME%20PAGE/smartSpeaker_dvzmp3.png",
-    productName: "samsung tv2",
-    description:
-      "Mini Speaker has a light weight and small size, which makes it easy to carry and use. A speaker that enables the user to listen to music.",
-    price: 3540,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "b3",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541587/FrambegTech/HOME%20PAGE/homeSpeaker_fylaiu.png",
-    productName: "samsung tv3",
-    description:
-      "The speaker features SmartThings Hub integration, allowing it to control other smart home appliances compatible.",
-    price: 4490,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "b4",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541602/FrambegTech/HOME%20PAGE/tablet_xfafto.png",
-    productName: "samsung tv4",
-    description:
-      "All Samsung Galaxy Tabs come with Android and One UI, front and rear facing cameras, microSD memory expansion and seamless.",
-    price: 2540,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "b5",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541594/FrambegTech/HOME%20PAGE/Phone_1_xezh4n.png",
-    productName: "samsung tv5",
-    description:
-      "Galaxy S22 Ultra smartphone offers a 108-megapixel camera and can shoot video in 8K ultra-high-definition resolution.",
-    price: 3440,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "b6",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541595/FrambegTech/HOME%20PAGE/Phone_2_fpfg1s.png",
-    productName: "samsung tv6",
-    description:
-      "Galaxy S22 Ultra smartphone offers a 108-megapixel camera and can shoot video in 8K ultra-high-definition resolution.",
-    price: 4590,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "b7",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541588/FrambegTech/HOME%20PAGE/Laptop_2_qrqcme.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541590/FrambegTech/HOME%20PAGE/Laptop_5_zmu5zi.png",
-    productName: "samsung tv7",
-    description:
-      "HP 15s (15s-Eq2143au) is a Windows 11 laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels.",
-    price: 1440,
-    addToCart: "Add To Cart",
-  },
-  {
-    id: "b8",
-    productImage:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541590/FrambegTech/HOME%20PAGE/Laptop_5_zmu5zi.png",
-    productImage2:
-      "https://res.cloudinary.com/dmdnq9vh8/image/upload/v1711541588/FrambegTech/HOME%20PAGE/Laptop_2_qrqcme.png",
-    productName: "samsung tv8",
-    description:
-      "HP 15s (15s-Eq2143au) is a Windows 11 laptop with a 15.60-inch display that has a resolution of 1920x1080 pixels.",
-    price: 2540,
-    addToCart: "Add To Cart",
-  },
-];
 
 const Home = () => {
   const [searchedWord, setSearchedWord] = useState("");
   const [checkSearchedWord, setCheckedSearchedWord] = useState(false);
+  const [bestDealsProducts, setBestDealsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const filteredProducts = HomePageProducts.filter((product) =>
+  const fetchBestDealsProducts = async (page) => {
+    const category = "Home";
+    try {
+      const response = await axios.get(
+        ` http://localhost:8080/products/BestDealsProducts/${category}?page=${page}`
+      );
+
+      const { products, totalPages } = response.data;
+      setBestDealsData(products);
+      setTotalPages(totalPages);
+    } catch (error) {
+      console.log("Error Fetching Products", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBestDealsProducts(currentPage + 1);
+  }, [currentPage]);
+
+  const filteredProducts = bestDealsProducts.filter((product) =>
     product.productName.toLowerCase().includes(searchedWord.toLowerCase())
   );
   return (
@@ -126,17 +65,41 @@ const Home = () => {
 
         <BrandDeals />
 
-        {checkSearchedWord && filteredProducts.length === 0 ? (
-          <div className="ml-16 lg:w-4/5 lg:m-auto">
-            <h1 className="text-xl">No results for {searchedWord}</h1>
-            <p>Try checking your spelling or use more general terms</p>
+        {filteredProducts.length === 0 ? (
+          <div className="flex justify-center items-center w-full">
+            <Lottie
+              className="w-[6rem]"
+              animationData={loadingAnimation}
+              loop={true}
+            />
           </div>
         ) : (
-          <TodaysBestDeals
-            HomePageProducts={
-              checkSearchedWord ? filteredProducts : HomePageProducts
-            }
-          />
+          <div className="lg:w-4/5 lg:m-auto">
+            <div className="lg:flex font-bold mb-8 m-4">
+              <h1 className="text-2xl opacity-75">Today's best deal</h1>
+              <Link
+                to="/TodaysDeals"
+                className="text-blue-600 text-xl hover:-translate-x-1 hover:scale-110 duration-300 lg:ml-5 lg:text-xl"
+              >
+                see more
+              </Link>
+            </div>
+            <div className="grid grid-cols-2 mb-8 gap-x-2 gap-y-8 md:grid-cols-3 md:mx-4 lg:grid-cols-4">
+              {filteredProducts?.map((todaysDeals) => (
+                <ProductItem
+                  key={todaysDeals._id}
+                  id={todaysDeals._id}
+                  productImage={todaysDeals.productImage}
+                  productImage2={todaysDeals.productImage2}
+                  productName={todaysDeals.productName}
+                  description={todaysDeals.description}
+                  price={todaysDeals.price}
+                  discount={todaysDeals.discount}
+                  addToCart={todaysDeals.addToCart}
+                />
+              ))}
+            </div>
+          </div>
         )}
 
         <TopBrands />
--- src/pages/page/homePage/TodaysBestDeals.js
+++ src/pages/page/homePage/TodaysBestDeals.js
@@ -1,38 +1,37 @@
-import { Fragment } from "react";
-import { Link } from "react-router-dom";
+// import { Fragment } from "react";
 
-import ProductItem from "../../components/ProductItem";
+// import ProductItem from "../../components/ProductItem";
 
-const TodaysBestDeals = ({ HomePageProducts }) => {
-  return (
-    <Fragment>
-      <section className="mt-10 m-4 border-2 shadow-md lg:w-4/5 lg:m-auto">
-        <div className="lg:flex font-bold mb-8 m-4">
-          <h1 className="text-2xl opacity-75">Today's best deal</h1>
-          <Link
-            to="/TodaysDeals"
-            className="text-blue-600 text-xl hover:-translate-x-1 hover:scale-110 duration-300 lg:ml-5 lg:text-xl"
-          >
-            see more
-          </Link>
-        </div>
-        <div className="grid grid-cols-2 mb-8 gap-x-2 gap-y-8 md:grid-cols-3 md:mx-4 lg:grid-cols-4">
-          {HomePageProducts?.map((product) => (
-            <ProductItem
-              key={product.id}
-              id={product.id}
-              productImage={product.productImage}
-              productImage2={product.productImage2}
-              description={product.description}
-              productName={product.productName}
-              price={product.price}
-              addToCart={product.addToCart}
-            />
-          ))}
-        </div>
-      </section>
-    </Fragment>
-  );
-};
+// const TodaysBestDeals = ({ HomePageProducts }) => {
+//   return (
+//     <Fragment>
+//       <section className="mt-10 m-4 border-2 shadow-md lg:w-4/5 lg:m-auto">
+//         <div className="lg:flex font-bold mb-8 m-4">
+//           <h1 className="text-2xl opacity-75">Today's best deal</h1>
+//           <Link
+//             to="/TodaysDeals"
+//             className="text-blue-600 text-xl hover:-translate-x-1 hover:scale-110 duration-300 lg:ml-5 lg:text-xl"
+//           >
+//             see more
+//           </Link>
+//         </div>
+//         <div className="grid grid-cols-2 mb-8 gap-x-2 gap-y-8 md:grid-cols-3 md:mx-4 lg:grid-cols-4">
+//           {HomePageProducts?.map((product) => (
+//             <ProductItem
+//               key={product.id}
+//               id={product.id}
+//               productImage={product.productImage}
+//               productImage2={product.productImage2}
+//               description={product.description}
+//               productName={product.productName}
+//               price={product.price}
+//               addToCart={product.addToCart}
+//             />
+//           ))}
+//         </div>
+//       </section>
+//     </Fragment>
+//   );
+// };
 
-export default TodaysBestDeals;
+// export default TodaysBestDeals;
